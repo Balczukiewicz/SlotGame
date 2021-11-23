c764a0eef80c97baa7b4125b5969a5d0b97d1c1e
Stanisław Balczukiewicz <57411061+Balczukiewicz@users.noreply.github.com>
2021-11-24 00:23:14 +0100

    adding java files

---
+++ src/SlotGame.java
@@ -0,0 +1,20 @@
+public class SlotGame {
+
+    public static void main( String[] args ) {
+        System.out.println( "Zaczynamy gre" );
+        SlotMachine slotMachine = new SlotMachine();
+        double sumOfAllBets = 0;
+        double sumOfAllPayouts = 0;
+        double rtp;
+        for ( int i = 0; i < 100000; i++ ) {
+            slotMachine.startingDisplay();
+            sumOfAllPayouts = slotMachine.sumOfAllWinnings();
+            sumOfAllBets += 50;
+            
+        }
+        System.out.println( "BONUS POINTS FOR RTP" );
+        rtp = (sumOfAllPayouts/sumOfAllBets);
+        System.out.println(rtp);
+
+    }
+}
